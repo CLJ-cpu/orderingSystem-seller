44765bcd9aff2fbee20bb211c42941c92d6e2223
xchen <1553101450@qq.com>
2022-03-20 16:29:33 +0800

    less配置

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavBar, Divider } from 'antd-mobile'
-import * as styles from './index.less' 
+import styles from './index.less'
 const Index = () => {
-   return (<div>
+   const [passwordLogin, setPasswordLogin] = useState(true)
+   useEffect(() => {
+
+   }, [])
+   return (<div className={styles.home}>
       <NavBar
          back={null}
-         right={<a>开店</a>}
+         right={<a style={{color:'orange',fontSize:'16px'}} >开店</a>}
          style={{
             '--border-bottom': '1px #eee solid'
          }}>登录</NavBar>
-      <span className={styles.loginMethod}>密码登录</span>|
-      <span>验证码登录</span>
+      <div className={styles.login}>
+         <span
+            className={passwordLogin ? styles.active : null}
+            onClick={() => setPasswordLogin(true)}
+         >
+            密码登录
+         </span>&nbsp;&nbsp;|&nbsp;&nbsp;
+         <span
+            className={passwordLogin ? null : styles.active}
+            onClick={() => setPasswordLogin(false)}
+         >
+            验证码登录
+         </span>
+      </div>
       <Divider />
+      <div className={styles.content}>
+         {passwordLogin&&1}
+      </div>
    </div>)
 }
 export default Index
